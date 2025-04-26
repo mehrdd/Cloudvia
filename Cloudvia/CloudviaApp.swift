e64d041d06fcdb6165d690a19a1e1141738499d9
m.nassiri <m.nassiri@vandaw.com>
2025-04-26 18:38:00 +0330

    Initial Commit

---
+++ Cloudvia/CloudviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudviaApp.swift
+//  Cloudvia
+//
+//  Created by Mehrdad Nassiri on 2025/4/26.
+//
+
+import SwiftUI
+
+@main
+struct CloudviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CloudviaTests/CloudviaTests.swift
@@ -0,0 +1,16 @@
+//
+//  CloudviaTests.swift
+//  CloudviaTests
+//
+//  Created by Mehrdad Nassiri on 2025/4/26.
+//
+
+import Testing
+
+struct CloudviaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
